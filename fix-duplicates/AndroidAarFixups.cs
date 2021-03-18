ed5363eeb7ad21773afda1ff5f89887c4f867415
Tuğçe Arar <stugcearar@outlook.com>
2021-03-18 21:42:47 +0300

    Update AndroidAarFixups.cs

--- fix-duplicates/AndroidAarFixups.cs
+++ fix-duplicates/AndroidAarFixups.cs
@@ -27,7 +27,7 @@ namespace AarLibsFix
                     {
 
                         // UGLY WORKAROUND
-                        // In the some of the native Huawei libraries, there exist multiple .aar files which have a libs/r-classes.jar file.
+                        // In the some of the native libraries, there exist multiple .aar files which have a libs/r-classes.jar file.
                         // In Xamarin.Android, there is a Task "CheckDuplicateJavaLibraries" which inspects jar files being pulled in from .aar files
                         // in assemblies to see if there exist any files with the same name but different content, and will throw an error if it finds any.
                         // However, for us, it is perfectly valid to have this scenario and we should not see an error.
