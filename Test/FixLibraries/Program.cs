301da6d51b987178c007eb1646d2b7095944cfb2
Tuğçe Arar <stugcearar@outlook.com>
2021-03-19 09:13:37 +0300

    Update Program.cs

--- Test/FixLibraries/Program.cs
+++ Test/FixLibraries/Program.cs
@@ -7,8 +7,8 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            AndroidAarFixups.FixupAarClass(@"D:\source\Github\fix-duplicates\Test\Aars\library1.aar", "lib-1");
-            AndroidAarFixups.FixupAarClass(@"D:\source\Github\fix-duplicates\Test\Aars\library2.aar", "lib-2");
+            AndroidAarFixups.FixupAarClass(@"..\library1.aar", "lib-1");
+            AndroidAarFixups.FixupAarClass(@"..\library2.aar", "lib-2");
         }
     }
 }
